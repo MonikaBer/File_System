899f0bcddc83a1f26db0d4713a7d63ded1c98ce6
MonikaBer <berlinska.monika@outlook.com>
2020-05-17 14:42:54 +0200

    correction of warnings

--- SimpleFS.cpp
+++ SimpleFS.cpp
@@ -8,40 +8,40 @@ SimpleFS::SimpleFS(std::string && config) {
 
 
 int SimpleFS::create(std::string && name, int mode) {
-
+    return -1;
 }
 
 
 int SimpleFS::open(std::string && name, int mode) {
-
+    return -1;
 }
 
 
 int SimpleFS::read(int fd, char * buf, int len) {
-
+    return -1;
 }
 
 
 int SimpleFS::write(int fd, char * buf, int len) {
-
+    return -1;
 }
 
 
 int SimpleFS::lseek(int fd, int whence, int offset) {
-
+    return -1;
 }
 
 
 int SimpleFS::unlink(std::string && name) {
-
+    return -1;
 }
 
 
 int SimpleFS::mkdir(std::string && name) {
-
+    return -1;
 }
 
 
 int SimpleFS::rmdir(std::string && name) {
-
+    return -1;
 }
