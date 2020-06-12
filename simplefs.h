e25864b10478fd931ecad9b1f5462bda5487e4ad
Artur Wyrozebski <01133391@pw.edu.pl>
2020-06-11 18:16:06 -0700

    Change function names

--- simplefs.h
+++ simplefs.h
@@ -2,14 +2,14 @@
 #define SIMPLEFS_H
 
 //extern "C" {
-    int create(char *name, int mode);
-    int open(char *name, int mode);
-    int read(int fd, char *buf, int len);
-    int write(int fd, char *buf, int len);
-    int lseek(int fd, int whence, int offset);
-    int unlink(char *name);
-    int mkdir(char *name);
-    int rmdir(char *name);
+    int simplefs_creat(char *name, int mode);
+    int simplefs_open(char *name, int mode);
+    int simplefs_read(int fd, char *buf, int len);
+    int simplefs_write(int fd, char *buf, int len);
+    int simplefs_lseek(int fd, int whence, int offset);
+    int simplefs_unlink(char *name);
+    int simplefs_mkdir(char *name);
+    int simplefs_rmdir(char *name);
 //}
 
 #endif
